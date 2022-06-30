df07c6d9486604f7850b00ca6a2aecb2874dbef7
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2022-06-30 20:22:38 +0900

    btn 에 shape square 추가

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -2,25 +2,11 @@ import React from 'react';
 import './button.scss';
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
+  shape?: 'square' | 'circle';
   onClick?: () => void;
 }
 
@@ -32,13 +18,14 @@ export const Button = ({
   size = 'medium',
   backgroundColor,
   label,
+  shape,
   ...props
 }: ButtonProps) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', `storybook-button--${size}`, mode, shape].join(' ')}
       style={{ backgroundColor }}
       {...props}
     >
